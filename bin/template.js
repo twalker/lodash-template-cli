e01760aa7fe32e5db2ddc3ccb712f47135dd4358
tim walker <tim.twalker@gmail.com>
2015-04-11 14:30:48 -0700

    initial commit

---
+++ bin/template.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+var program = require('commander'),
+    Promise = require('promise'),
+    template = require('lodash.template'),
+    pkg = require('../package.json'),
+    fs = require('fs'),
+    read = Promise.denodeify(fs.readFile);
+
+program
+  .version(pkg.version)
+  .option('-f, --file [path]', 'Template file')
+  .option('-j, --json [path]', 'Json file', {})
+  .option('-o, --out [path]', 'Output file')
+  .parse(process.argv);
+
+if(!program.file || !program.json) program.help();
+
+// Support output to stdout or file
+var ws = (program.out) ? fs.createWriteStream(program.out) : process.stdout;
+
+Promise
+  .all([read(program.file), read(program.json)])
+  .then(function(files){
+    return template(files[0])(JSON.parse(files[1]));
+  })
+  .then(function(html){
+    ws.write(html);
+    process.exit(0);
+  })
+  .catch(function(err){
+    process.stderr.write(err.stack + '\n')
+    process.exit(err.errno);
+  });
---
+++ readme.md
@@ -0,0 +1,25 @@
+#lodash-template-cli
+Run lodash.template from the command line.
+
+```
+Usage: template [options]
+
+Options:
+
+  -f, --file [path]  Template file
+  -j, --json [path]  Json file
+  -o, --out [path]   Output file
+
+```
+
+
+
+TODO:
+
+- template should just be first arg
+- pick a name
+  + template
+  + _template
+  + lodash-template
+
+- test
